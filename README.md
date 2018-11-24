d784125452a90d1ae14899b55643d33cbccc58a3
chimame <rito.tamata@gmail.com>
2018-11-24 13:19:32 +0900

    added so that searchResults can be acquired

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ```bash
 $ npm install
 $ docker run --rm -v $(pwd):/var/task lambci/lambda:build-nodejs8.10
-$ docker run --rm -v $(pwd):/var/task lambci/lambda:nodejs8.10  index.handler '{"searchWord": "hoge+hoge", "siteDomain": "example.com"}'
+
+# if getting searchResults
+$ docker run --rm -v $(pwd):/var/task lambci/lambda:nodejs8.10  index.handler '{"searchWord": "hoge hoge", "siteDomain": "example.com"}' | jq '.searchResults'
+
+# if getting screenshot
+$ docker run --rm -v $(pwd):/var/task lambci/lambda:nodejs8.10  index.handler '{"searchWord": "hoge hoge", "siteDomain": "example.com"}' | jq '.screenShot' | xxd  -r -p > $(pwd)/image.png
 ```
--- index.js
+++ index.js
@@ -22,30 +22,33 @@ exports.handler = async (event, context, callback) => {
     browser = await puppeteer.connect({ 
       browserWSEndpoint: (await CDP.Version()).webSocketDebuggerUrl 
     })
-    const context = browser.defaultBrowserContext();
+    const context = browser.defaultBrowserContext()
 
     // 初期設定
-    await context.overridePermissions('https://www.google.co.jp', ['geolocation'])
     const page = await context.newPage()
     await page.setExtraHTTPHeaders({ 'Accept-Language': 'ja' })
-    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
+    // ユーザエージェントを設定する場合はこのように設定
+    //await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36')
     await page.goto(`https://www.google.co.jp/`, { waitUntil: 'networkidle0' })
     // ロケーション許可して疑似ロケーションで上書き
-    await context.overridePermissions('https://www.google.co.jp', ['geolocation'])
-    await page.setGeolocation({ longitude: event.longitude, latitude: event.latitude, accuracy: 50 })
+    //await context.overridePermissions('https://www.google.co.jp', ['geolocation'])
+    //await page.setGeolocation({ longitude: event.longitude, latitude: event.latitude, accuracy: 50 })
 
     // 検索操作
     const searchWord = encodeURIComponent(event.searchWord)
     await page.goto(`https://www.google.co.jp/search?q=${searchWord}`, { waitUntil: 'domcontentloaded' })
-    try {
-        const location = await page.evaluate(() => new Promise(resolve => navigator.geolocation.getCurrentPosition(position => {
-            resolve({latitude: position.coords.latitude, longitude: position.coords.longitude});
-        })))
-        console.log(location)
-    } catch (positionError) {
-        console.error(positionError)
-    }
+    // ロケーションが設定されているか確認
+    //try {
+    //    const location = await page.evaluate(() => new Promise(resolve => navigator.geolocation.getCurrentPosition(position => {
+    //        resolve({latitude: position.coords.latitude, longitude: position.coords.longitude})
+    //    })))
+    //    console.log(location)
+    //} catch (positionError) {
+    //    console.error(positionError)
+    //}
     // Webフォントを適用して豆腐を回避
+    // If User Agent is not set, the font will not be reflected, so User Agent must be set
     await page.evaluate(() => {
         var style = document.createElement('style')
         style.textContent = `
@@ -54,10 +57,21 @@ exports.handler = async (event, context, callback) => {
             div, input, a{ font-family: 'Noto Sans JP', sans-serif !important; };`
         document.head.appendChild(style)
     })
-    await page.waitFor(1000) // ちょっとダサ
-    const screenShot = await page.screenshot({fullPage: true});
+    await page.waitFor(1000) // Wait until the font is reflected
+    const screenShot = await page.screenshot({fullPage: true})
+    const searchResults = await page.evaluate(() => {
+      const ret = []
+      const nodeList = document.querySelectorAll("div#search h3")
+
+      nodeList.forEach(node => {
+        ret.push(node.innerText)
+      })
+
+      return ret
+    })
+
     await page.waitFor(1000)
-    return callback(null, screenShot.toString('hex'))
+    return callback(null, {result: 'OK', searchResults: searchResults, screenShot: screenShot.toString('hex')})
   } catch (err) {
     console.error(err)
     return callback(null, JSON.stringify({ result: 'NG' }))
